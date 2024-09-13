0c850d6b914eab15074caee317fce4ac42303548
Eduardo Lira <eduardoliracunha17@gmail.com>
2024-09-13 12:26:46 -0400

    Ajustando texto do banner

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,33 +1,35 @@
 import styles from "./Banner.module.css";
-import circuloColorido from 'assets/circulo_colorido.png'
-import minhaFoto from 'assets/eu.jpeg'
+import circuloColorido from "assets/circulo_colorido.png";
+import minhaFoto from "assets/eu.jpeg";
 const Banner = () => {
-    return (
-        <div className={styles.banner}>
-            <div className={styles.apresentacao}>
-                <h1 className={styles.title}>Olá mundo !</h1>
+  return (
+    <div className={styles.banner}>
+      <div className={styles.apresentacao}>
+        <h1 className={styles.title}>Olá mundo !</h1>
 
-                <p className={styles.paragrafo}>
-                    Boas vindas ao meu espaço pessoal! Eu sou Eduardo Lira,
-                    estou praticando React-Router atráves da Alura. Estou achando o react-router-dom muito eficiente !!!
-                </p>
-            </div>
+        <p className={styles.paragrafo}>
+          Boas-vindas ao meu espaço pessoal! Eu sou Eduardo Lira, estudante de
+          programação focado em desenvolvimento web. Aqui, compartilho vários
+          conhecimentos e experiências, espero que você aprenda algo novo!
+        </p>
+      </div>
 
-            <div className={styles.imagens}> 
-              <img 
-                src={circuloColorido} 
-                aria-hidden={true}
-                className={styles.circuloColorido} 
-              />
+      <div className={styles.imagens}>
+        <img
+          src={circuloColorido}
+          alt="Circulo colorido para ser o background da imagem do Eduardo sorrindo"
+          aria-hidden={true}
+          className={styles.circuloColorido}
+        />
 
-              <img 
-                src={minhaFoto} 
-                className={styles.minhaFoto}
-                alt="Foto do Eduardo Sorrindo"
-              />
-            </div>
-        </div>
-    );
+        <img
+          src={minhaFoto}
+          className={styles.minhaFoto}
+          alt="Foto do Eduardo Sorrindo"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
